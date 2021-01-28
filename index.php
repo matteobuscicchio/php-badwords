90b0c8fb230eb45fe0e6056f4338a960ca3b3dc1
matteobuscicchio <matteoezio@me.com>
2021-01-28 17:59:28 +0100

    Richiesta get implementata
    Ora aggiungendo la richiesta "/index.php?replace=Lorem"

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
     </head>
     <body>
         <?php
-            $paragrafo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-            $badWord = 'Lorem';
-            $replace = str_replace($badWord, '***', $paragrafo);
+            $paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+            $replace = str_replace($_GET['replace'], '***', $paragraph);
         ?>
 
+        <span> Lunghezza paragrafo originale: <?php echo strlen($paragraph);?>. </span>
+        
         <p> <?php echo $replace;?> </p>
 
-        <span>Lunghezza paragrafo: <?php echo strlen($paragrafo);?>.</span>
+        <span> Lunghezza paragrafo con sostituzione: <?php echo strlen($replace);?>. </span>
     </body>
 </html>
